a2b026710291fef1450c95f1ffea62b7294e0cb7
Jamshid <jamshidsherkulov1@gmail.com>
2025-08-07 12:35:04 +0100

    Date elements has been fixed

--- src/main/java/com/tasktracker/LocalDateTimeAdapter.java
+++ src/main/java/com/tasktracker/LocalDateTimeAdapter.java
@@ -7,16 +7,19 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class LocalDateTimeAdapter implements JsonSerializer<LocalDateTime>, JsonDeserializer<LocalDateTime> {
-    private static final DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
 
     @Override
     public LocalDateTime deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
-
-        return null;
+        String dateStr = jsonElement.getAsString();
+        if (dateStr.contains(".")) {
+            dateStr = dateStr.substring(0, dateStr.indexOf("."));
+        }
+        return LocalDateTime.parse(dateStr, formatter);
     }
 
     @Override
     public JsonElement serialize(LocalDateTime localDateTime, Type type, JsonSerializationContext jsonSerializationContext) {
-        return null;
+        return new JsonPrimitive(localDateTime.format(formatter));
     }
 }
